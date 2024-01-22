24b97e44a65b339c5d88bacda269d5245e19681c
chotk <Chotkiynambo1@gmail.com>
2024-01-22 18:32:52 +0500

    task.1 Умрихин Александр

--- task1.py
+++ task1.py
@@ -1,8 +1,14 @@
-class task1:
-    def __init__(self):
-        pass
-    def giveEvenNumbers(self, arg):
-        '''
-        :param arg: список целых чисел
-        :return: список четных чисел
-        '''
+def __init__(self):
+    pass
+
+
+def giveEvenNumbers(self, arg):
+    '''
+    :param arg: список целых чисел
+    :return: список четных чисел
+    '''
+    chisl = list()
+    for num in arg:
+        if num % 2 == 0:
+            chisl.append(num)
+    return chisl
