a299364a679a00a420027408d116bfccf3bc9fb0
Михаил <MKX0506@mail.ru>
2024-01-22 16:46:10 +0300

    task1 Хандогин Михаил

--- task1.py
+++ task1.py
@@ -2,7 +2,9 @@ class task1:
     def __init__(self):
         pass
     def giveEvenNumbers(self, arg):
-        '''
-        :param arg: список целых чисел
-        :return: список четных чисел
-        '''
+        evnum = list()
+        for i in arg:
+            if i % 2 == 0:
+                evnum.append(i)
+        return evnum
+
