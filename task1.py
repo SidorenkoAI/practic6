3bb0e76d822c59530454a3e472c9b86969d1cd21
masle <masle@gmail.com>
2024-01-22 16:37:44 +0300

    Task1

--- task1.py
+++ task1.py
@@ -2,7 +2,8 @@ class task1:
     def __init__(self):
         pass
     def giveEvenNumbers(self, arg):
-        '''
-        :param arg: список целых чисел
-        :return: список четных чисел
-        '''
+        numbers = list()
+        for num in arg:
+            if num % 2 == 0:
+                numbers.append(num)
+        return numbers
