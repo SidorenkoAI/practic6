24a6ff1cb09e2e0b585855a5eec808411c3741f8
chotk <Chotkiynambo1@gmail.com>
2024-01-22 19:02:03 +0500

    task.2 Умрихин Александр

--- task2.py
+++ task2.py
@@ -2,6 +2,13 @@ class task2:
     def __init__(self):
         pass
     def greater(self, arg):
+        vozr = list()
+        for num in len(arg):
+            if arg[num] > arg[num+1]:
+                print("всё норм")
+            else: print("не то")
+
+
         '''
         :param arg: список целых чисел
         :return: Если arg монотонно возрастает
