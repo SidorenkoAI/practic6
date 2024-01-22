916e9a2fe9df1f2c3bfee6ecbf225a1ea45ae1b5
task-1-Воронин-Роман <dotykartinka@gmail.com>
2024-01-22 20:19:36 +0700

    task 1 Воронин Роман

--- task1.py
+++ task1.py
@@ -2,6 +2,7 @@ class task1:
     def __init__(self):
         pass
     def giveEvenNumbers(self, arg):
+        print('я сделал')
         '''
         :param arg: список целых чисел
         :return: список четных чисел
