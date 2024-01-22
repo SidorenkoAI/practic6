bd07db14db7f311394b1b5d8a6c60cf678e7033b
Николай <nzverzdinov@gmail.com>
2024-01-22 20:18:37 +0700

    task1 Зверздинов Николай

--- task1.py
+++ task1.py
@@ -2,6 +2,8 @@ class task1:
     def __init__(self):
         pass
     def giveEvenNumbers(self, arg):
+        print('Я сделал ураа!!')
+
         '''
         :param arg: список целых чисел
         :return: список четных чисел
