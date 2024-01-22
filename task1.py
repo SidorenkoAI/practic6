f5ce7acd5b90823e9ad812a6b3305ff7db72daff
Илья Суслов <suslovila292@gmail.com>
2024-01-22 16:42:35 +0300

    готово

--- task1.py
+++ task1.py
@@ -2,7 +2,28 @@ class task1:
     def __init__(self):
         pass
     def giveEvenNumbers(self, arg):
-        '''
-        :param arg: список целых чисел
-        :return: список четных чисел
-        '''
+       list_res = []
+       for num in arg:
+           if num % 2 == 0:
+               list_res.append(num)
+       return list_res
+
+
+Tasked = task1
+print(Tasked.giveEvenNumbers(self= 0 , arg= [2, 5, 1, 3, 6, 7, 3, 4]))
+print("Усе :)")
+
+
+
+
+
+
+
+
+
+
+
+'''
+:param arg: список целых чисел
+:return: список четных чисел
+'''
