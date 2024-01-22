bae410bc7616cf690a58d90a215ccc26a3e87fd8
USER <simakovadarya07@gmail.com>
2024-01-22 17:31:33 +0300

    task2 Симакова Дарья

--- task2.py
+++ task2.py
@@ -2,6 +2,15 @@ class task2:
     def __init__(self):
         pass
     def greater(self, arg):
+        if len(arg) == 0:
+            return False
+        res = True
+        for i in range(len()):
+            if arg[i] >= arg[i+1]:
+                res = False
+        return res
+
+
         '''
         :param arg: список целых чисел
         :return: Если arg монотонно возрастает
