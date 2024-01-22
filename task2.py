e294376f243abc4b0d88bde4b1f010ebebea7c5f
TheGotosky <belyaevdavid2009@gmail.com>
2024-01-22 19:03:11 +0500

    task2 Беляев Давидиккк

--- task2.py
+++ task2.py
@@ -2,9 +2,8 @@ class task2:
     def __init__(self):
         pass
     def greater(self, arg):
-        '''
-        :param arg: список целых чисел
-        :return: Если arg монотонно возрастает
-        (то есть каждый элемент больше предыдущего),
-        то вернуть True, иначе False
-        '''
+        for i in arg:
+             if i < arg[i]:
+                 print(True)
+             else:
+                 print(False)
