ebbd8b87d7f6228a5008936d4c4ae566551350ad
USER <simakovadarya07@gmail.com>
2024-01-22 16:39:01 +0300

    task1 Симакова Дарья

--- task1.py
+++ task1.py
@@ -2,7 +2,13 @@ class task1:
     def __init__(self):
         pass
     def giveEvenNumbers(self, arg):
-        '''
-        :param arg: список целых чисел
+        spisok = list()
+        for num in arg:
+            if num % 2 == 0:
+                spisok.append(num)
+        return spisok
+
+
+        ''':param arg: список целых чисел
         :return: список четных чисел
         '''
