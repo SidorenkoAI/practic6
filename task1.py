3350917a5fece903ab94d0d0d807bcbdc0e0d65e
task-1-Воронин-Роман <dotykartinka@gmail.com>
2024-01-22 20:23:30 +0700

    task 1 Воронин Роман

--- task1.py
+++ task1.py
@@ -1,9 +1,17 @@
-class task1:
+class Task1:
     def __init__(self):
         pass
+
     def giveEvenNumbers(self, arg):
-        print('я сделал')
         '''
         :param arg: список целых чисел
         :return: список четных чисел
         '''
+        even_numbers = [num for num in arg if num % 2 == 0]
+        return even_numbers
+
+task = Task1()
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+result = task.giveEvenNumbers(numbers)
+print(result)
