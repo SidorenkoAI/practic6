2598ae00cf07d91e6e91005ba95307b75771c075
Paladin4ick <ivandiubko@gmail.com>
2024-01-22 20:00:27 +0600

    task1 task4 Дюбко Иван

--- task2.py
+++ task2.py
@@ -2,9 +2,14 @@ class task2:
     def __init__(self):
         pass
     def greater(self, arg):
-        '''
+        """
         :param arg: список целых чисел
         :return: Если arg монотонно возрастает
         (то есть каждый элемент больше предыдущего),
         то вернуть True, иначе False
-        '''
+        """
+        i = 1
+        while i <= len(arg) - 1 and arg[i] > arg[i - 1]:
+            i += 1
+        return 'YES' if i == len(arg) else 'NO'
+        
--- task4.py
+++ task4.py
@@ -4,3 +4,7 @@ def PalindromeFilter(arg, minLength):
     :return: список строк, которые являются
     палиндромами и имеют длину не меньше minLength
     '''
+    arg = arg.replace(' ','').lower()
+    return arg if (arg == arg[::-1] and len(arg) > minLength) else "False"
+a = PalindromeFilter("калаш", 5)
+print(a)
