61e17c2b6ad3ad9d3b866bc03ead16976da80aec
SidorenkoAI <80513827+SidorenkoAI@users.noreply.github.com>
2024-01-23 17:32:48 +0700

    Revert "Task5 барышников ефим"

--- task1.py
+++ task1.py
@@ -2,8 +2,7 @@ class task1:
     def __init__(self):
         pass
     def giveEvenNumbers(self, arg):
-        EvNum = list()
-        for num in arg:
-            if num % 2 == 0:
-                EvNum.append(num)
-        return EvNum
+        '''
+        :param arg: список целых чисел
+        :return: список четных чисел
+        '''
